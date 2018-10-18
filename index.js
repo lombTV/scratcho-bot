100a290abeb98d0afd8ffed9d899acc0774d080e
lombTV <televisiondart@gmail.com>
2018-10-17 21:14:43 -0400

    Oh god i hope this works so badly

--- index.js
+++ index.js
@@ -44,19 +44,19 @@ client.on("message", async message => {
 	let args = messageArray.slice(1); // Whatever the argument is
 
 	let commandfile = client.commands.get(cmd.slice(prefix.length));
-	if (commandfile) commandfile.run(bot, message, args);
+	if (commandfile) commandfile.run(client, message, args);
 
-	// /* Return Uptime to User */
+	/* Return Uptime to User */
 
-	// if (cmd === `uptime`) {
-	// 	let totalSeconds = (client.uptime / 1000);
-	// 	let hours = Math.floor(totalSeconds / 3600);
-	// 	totalSeconds %= 3600;
-	// 	let minutes = Math.floor(totalSeconds / 60);
-	// 	let seconds = totalSeconds % 60;
-	// 	let uptime = `I've been online for ${hours} hours, ${minutes} minutes and ${seconds} seconds.`;
-	// 	return message.channel.send(uptime);
-	// }
+	if (cmd === `uptime`) {
+		let totalSeconds = (client.uptime / 1000);
+		let hours = Math.floor(totalSeconds / 3600);
+		totalSeconds %= 3600;
+		let minutes = Math.floor(totalSeconds / 60);
+		let seconds = totalSeconds % 60;
+		let uptime = `I've been online for ${hours} hours, ${minutes} minutes and ${seconds} seconds.`;
+		return message.channel.send(uptime);
+	}
 
 	// /* Return Bot Information */
 	// if (cmd === `botinfo`) {

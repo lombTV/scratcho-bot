045ebc8e87f74853d7654f94dfb0de957f1a9dce
lombTV <televisiondart@gmail.com>
2018-10-17 22:40:37 -0400

    Fixed repeat

--- commands/repeat.js
+++ commands/repeat.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-	return message.channel.send(args);
+	return message.channel.send(args.join(" "));
 }
 
 module.exports.help = {

1dba1dbdf6a9e8e0128aa2b3fd285e0bd28fc772
lombTV <televisiondart@gmail.com>
2018-10-17 22:02:23 -0400

    Split help.js into separate file

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,4 @@
 const Discord = require("discord.js");
-const botconfig = require("./botconfig.json");
-
 
 module.exports.run = async (client, message, args) => {
 		console.log("Running help command...");
@@ -8,12 +6,12 @@ module.exports.run = async (client, message, args) => {
 		let botMsg = "Hi!";
 		botMsg += " My name's SCRATCH-O, and I'm a utility bot for private use written by lombtv!";
 		botMsg += "\n``--Information--";
-		botMsg += `\n${prefix}help: Shows you all available commands.`;
-		botMsg += `\n${prefix}uptime: Shows how long I've been running.`;
-		botMsg += `\n${prefix}botinfo: Information about the bot is stored here.`;
-		botMsg += `\n${prefix}serverinfo: Information about the server is stored here.`;
+		botMsg += `\nhelp: Shows you all available commands.`;
+		botMsg += `\nuptime: Shows how long I've been running.`;
+		botMsg += `\nbotinfo: Information about the bot is stored here.`;
+		botMsg += `\nserverinfo: Information about the server is stored here.`;
 		botMsg += "\n--Fun--";
-		botMsg += `\n${prefix}repeat: Repeats a message you pass.`;
+		botMsg += `\nrepeat: Repeats a message you pass.`;
 		botMsg += "\n``";
 		return message.channel.send(botMsg);
 }

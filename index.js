8832b80fcffbdb21f7355898e688e34d92b4f94d
lombTV <televisiondart@gmail.com>
2018-10-18 17:46:27 -0400

    Welcome Message

--- index.js
+++ index.js
@@ -29,10 +29,13 @@ client.on("ready", async () => {
 });
 
 client.on('guildMemberAdd', member => {
+	console.log("New Member Joined: " + member.user.username);
+	const channel = member.guild.channels.find("name", "general");
 	if (member.user.username.includes("discord.gg")) {
 		member.ban("Username contained a Discord Invite link.");
+		console.log("Member " + member.user.username + " with Discord Invite Link for a name has been banned.");
 	}
-	return message.channel.send("Welcome, " + member.user.username + "!");
+	return channel.send("Welcome, " + member.user.username + "!");
 });
 
 client.on("message", async message => {

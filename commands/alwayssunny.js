84bc1d5689767030fedb30865723cd1c5f39c5a6
lombTV <televisiondart@gmail.com>
2018-10-17 22:31:19 -0400

    Always Sunny added

--- commands/alwayssunny.js
+++ commands/alwayssunny.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 	let j = "\"" + args +"\"";
 	let b = Buffer.from(j, 'binary').toString('base64');
-	return message.channel.send(`http://alexanderlozada.com/iasip/?${s} :ok_hand:`);
+	return message.channel.send(`http://alexanderlozada.com/iasip/?${b} :ok_hand:`);
 }
 
 module.exports.help = {

76975ba0b2e5f98a7a4a14e65d1dafff1b64ddb4
lombTV <televisiondart@gmail.com>
2018-10-18 18:48:06 -0400

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 /* Returns ping time. */
 
 module.exports.run = async (client, message, args) => {
-	let msg = "Break, cmon. ``" + ${client.pings[0]} + " ms``";
-	return message.channel.send(msg);
+	const m = await message.channel.send("And stop.");
+    m.edit(`Scratch-o! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
 
 module.exports.help = {

0462162986de96dcc5d132c738f31a309f30e5af
lombTV <televisiondart@gmail.com>
2018-10-18 18:45:36 -0400

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 /* Returns ping time. */
 
 module.exports.run = async (client, message, args) => {
-	let msg = "Break, cmon. ``" + new Date().getTime() + message.createdTimestamp + " ms``";
+	let msg = "Break, cmon. ``" + ${client.pings[0]} + " ms``";
 	return message.channel.send(msg);
 }
 

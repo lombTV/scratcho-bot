a7a27743fff985418cda19654ce01449456938f8
lombTV <televisiondart@gmail.com>
2018-10-17 20:25:40 -0400

    Update botinfo

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ client.on("message", async message => {
 		.setDescription("SCRATCH-O Information")
 		.setColor("#dd0000")
 		.setThumbnail(boticon)
-		.addField("Bot Name", client.user.username);
-		.addField("Source Code", "https://github.com/lombTV/scratcho-bot")
+		.addField("Bot Name", client.user.username)
+		.addField("Source Code", "https://github.com/lombTV/scratcho-bot");
 
 		return message.channel.send(botembed);
 	}

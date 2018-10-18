4ca2402b82c6a1ea50a9e227f58d76a4cd004117
lombTV <televisiondart@gmail.com>
2018-10-17 21:10:46 -0400

    Oh god i hope this works so badly

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ client.commands = new Discord.Collection();
 fs.readdir("./commands/", (err, file) => {
 	/* Gets each .js file found in the commands folder and makes them a command in the bot. */
 	if (err) console.log(err);
-	let jsfile = files.filter(f => f.split(".").pop() === "js");
+
+	let jsfile = files.filter(f => f.split(".").pop() === "js")
 	if (jsfile.length <= 0) {
 		console.log("Could not find command files.");
 		return;
 	}
-	jsfile.foreach((f, i) => {
+
+	jsfile.forEach((f, i) => {
 		let props = require(`./commands/${f}`);
 		console.log(`${f} was loaded successfully.`);
 		client.commands.set(props.help.name, props);
+		
 	});
 
 });
@@ -43,52 +46,52 @@ client.on("message", async message => {
 	let commandfile = client.commands.get(cmd.slice(prefix.length));
 	if (commandfile) commandfile.run(bot, message, args);
 
-	/* Return Uptime to User */
+	// /* Return Uptime to User */
 
-	if (cmd === `uptime`) {
-		let totalSeconds = (client.uptime / 1000);
-		let hours = Math.floor(totalSeconds / 3600);
-		totalSeconds %= 3600;
-		let minutes = Math.floor(totalSeconds / 60);
-		let seconds = totalSeconds % 60;
-		let uptime = `I've been online for ${hours} hours, ${minutes} minutes and ${seconds} seconds.`;
-		return message.channel.send(uptime);
-	}
+	// if (cmd === `uptime`) {
+	// 	let totalSeconds = (client.uptime / 1000);
+	// 	let hours = Math.floor(totalSeconds / 3600);
+	// 	totalSeconds %= 3600;
+	// 	let minutes = Math.floor(totalSeconds / 60);
+	// 	let seconds = totalSeconds % 60;
+	// 	let uptime = `I've been online for ${hours} hours, ${minutes} minutes and ${seconds} seconds.`;
+	// 	return message.channel.send(uptime);
+	// }
 
-	/* Return Bot Information */
-	if (cmd === `botinfo`) {
-		let boticon = client.user.displayAvatarURL;
-		let botembed = new Discord.RichEmbed()
-		.setDescription("SCRATCH-O Information")
-		.setColor("#0000ff")
-		.setThumbnail(boticon)
-		.addField("Bot Name", client.user.username)
-		.addField("Source Code", "https://github.com/lombTV/scratcho-bot");
+	// /* Return Bot Information */
+	// if (cmd === `botinfo`) {
+	// 	let boticon = client.user.displayAvatarURL;
+	// 	let botembed = new Discord.RichEmbed()
+	// 	.setDescription("SCRATCH-O Information")
+	// 	.setColor("#0000ff")
+	// 	.setThumbnail(boticon)
+	// 	.addField("Bot Name", client.user.username)
+	// 	.addField("Source Code", "https://github.com/lombTV/scratcho-bot");
 
-		return message.channel.send(botembed);
-	}
+	// 	return message.channel.send(botembed);
+	// }
 
-	/* Return Server Information */
-	if (cmd === `serverinfo`) {
-		let servericon = message.guild.iconURL;
-		let serverembed = new Discord.RichEmbed()
-		.setDescription("Server Information")
-		.setColor("#ff00ff")
-		.setThumbnail(servericon)
-		.addField("Server Name", message.guild.name)
-		.addField("Created On ", message.guild.createdAt)
-		.addField("User Joined ", message.member.joinedAt)
-		.addField("Members: ", message.guild.memberCount)
-		.addField("Owner: ", message.guild.owner);
+	// /* Return Server Information */
+	// if (cmd === `serverinfo`) {
+	// 	let servericon = message.guild.iconURL;
+	// 	let serverembed = new Discord.RichEmbed()
+	// 	.setDescription("Server Information")
+	// 	.setColor("#ff00ff")
+	// 	.setThumbnail(servericon)
+	// 	.addField("Server Name", message.guild.name)
+	// 	.addField("Created On ", message.guild.createdAt)
+	// 	.addField("User Joined ", message.member.joinedAt)
+	// 	.addField("Members: ", message.guild.memberCount)
+	// 	.addField("Owner: ", message.guild.owner);
 
-		return message.channel.send(serverembed);
-	}
+	// 	return message.channel.send(serverembed);
+	// }
 
-	/* Repeat after me */
+	// /* Repeat after me */
 
-	if (cmd ===`repeat`) {
-		return message.channel.send(args);
-	}
+	// if (cmd ===`repeat`) {
+	// 	return message.channel.send(args);
+	// }
 
 });
 

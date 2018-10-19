f364868f2efa65e561f9bacfe89a27c7b0734a3e
lombTV <televisiondart@gmail.com>
2018-10-18 20:46:43 -0400

    Dynmaic prefix

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,28 +1,24 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const mysql = require('mysql');
+
 
 /* Let server owners change the prefix of the bot. */
 
 module.exports.run = async (client, message, args) => {
 	if (!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Sorry, you don't have permission to use this function.");
-	let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf-8"));
 
-	prefixes[message.guild.id] = {
-		prefixes: args[0]
-	};
-	console.log("Writing prefix...");
-	fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
-		if (err) console.log("Error! " + err)
-	});
+	
+	
 
 	let sEmbed = new Discord.RichEmbed()
 	.setColor("#0f0fdd")
 	.setTitle("Prefix Set!")
 	.setDescription(`Set to ${args[0]}`)
 
-	message.channel.send(sEmbed);
+	return message.channel.send(sEmbed);
 }
 
 module.exports.help = {
-	name: "botinfo"
+	name: "prefix"
 }
